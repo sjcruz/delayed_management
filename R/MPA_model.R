0c8138ea112b42bcee1390828bad85d64670843c
sjcruz <seleni@udel.edu>
2020-05-13 14:48:12 -0400

    Initial commit

---
+++ R/MPA_model.R
@@ -0,0 +1,95 @@
+#' Marine Reserve patch model  
+#' @param r is the intrinisc growth rate, output from the DLSA.R function 
+#' @param K is the fishery's carrying capcity, output from the DLSA.R function
+#' @param Fishing is the fishing effort, output from the DLSA.R function
+#' @param B is the available biomass, output from the DLSA.R function
+#' @param years number of years of projection
+#' @param MPA.mat matrix of 0 and 1; 0= marine reserve patches, 1= open access patches
+#' @param m.rate proportion of available biomass moving/spilling over
+#' @param MSY is the maximum sustainable yield, output from the DLSA.R function
+#' @param bmsy is the biomass that enables fishery to to deliver MSY, output from the DLSA.R function
+#' @param fmsy sit he fishing effort that enables a fishery to deliver MSY, output from the DLSA.R function 
+#' @param p is the price per metric ton ($/ MT)
+#' @param c is the cost associated with fishing 
+#' @param profit.msy is the profit that is made at MSY 
+#' @param start.year refers to the year in which the maine reseves are implemented 
+#' @return dataframe with biomass, catch, and profits made across all patches 
+
+
+MPA.Model <- function(Name, Adjusted, r, K, Fishing, B, years, MPA.mat, mrate, MSY, bmsy, fmsy, p, c, profit.msy, start.year){
+  source(here::here("R", "fishing_effort.R"))
+ 
+  patches <- 106 #matrix is 106 by 106 patches for a total of 11236 patches 
+  F.mat <- matrix(ncol=patches, nrow=patches, Fishing) 
+  K.mat <- matrix(ncol=patches, nrow=patches, K/11236)  
+  B.mat <- matrix(ncol=patches, nrow=patches, B/11236)  
+  arriving <- matrix(nrow=patches, ncol=patches, 0) 
+  procrastination <- max(start.year-2015, 0)
+  Years<- as.numeric(2015:(2015+ years + procrastination))
+  
+  l.patch <- as.numeric(c(patches, 1: (patches-1)))
+  r.patch <- as.numeric(c(2: patches, 1))
+  u.patch <- as.numeric(c(patches, 1: (patches-1)))
+  d.patch <- as.numeric(c(2: patches, 1))
+  
+  summary<- data.frame(Name=NA, 
+                       Adjusted=NA, 
+                       Year=NA,
+                       Catch=NA,
+                       Biomass=NA,
+                       bbmsy=NA,
+                       ffmsy=NA,
+                       PV=NA,
+                       Implementation_year=NA)
+
+  for (i in Years){
+    
+    if(i == start.year){F.mat = F.mat*MPA.mat} else {F.mat = F.mat}
+    
+    t <- i - 2015
+    
+    F.out <- fishing_effort (t=t, p=p, MSY=MSY, r=r, bmsy=bmsy, fmsy=fmsy, 
+                             F.mat=F.mat, B.mat=B.mat, c=c, profit.msy = profit.msy)
+    
+    F.mat <- as.matrix(F.out[[1]])
+    PV <-  as.matrix(F.out[[3]])
+    
+    leaving <- mrate*B.mat 
+    leaving[leaving < 0] <- 0
+    
+    for(row in 1:nrow(arriving)) {
+      for(col in 1:ncol(arriving)) {
+        
+        arriving [row, col] <-  0.25*leaving[row, l.patch[col]]+ 
+                                0.25*leaving[row, r.patch[col]] + 
+                                0.25*leaving[u.patch[row], col] + 
+                                0.25*leaving[d.patch[row], col]
+      }
+    } #close nested forloop for calculating arriving
+    
+    arriving[arriving < 0] <- 0
+    surplus <- r * B.mat *(1 - B.mat/K.mat)
+    surplus[surplus < 0] <- 0
+    catches <- F.mat * B.mat
+    catches[catches < 0] <- 0
+    B.mat <- B.mat + surplus - catches - leaving + arriving
+    B.mat[B.mat < 0] <- 0
+    
+    outside <- B.mat*MPA.mat
+   
+    output<- data.frame (Name = Name,
+                         Adjusted = Adjusted,
+                         Year= i,
+                         Catch = sum(catches),
+                         Biomass = sum(B.mat),
+                         bbmsy = sum(B.mat)/bmsy,
+                         ffmsy = mean (F.mat)/fmsy,
+                         PV = sum(PV), 
+                         Implementation_year=start.year)
+    
+    
+    
+    summary <- rbind (summary, output)
+  }
+  return(summary[-1,])
+}
---
+++ R/payoff.R
@@ -0,0 +1,98 @@
+#' Function calculates transition and payoff period compared to business as usual
+#' @param BAU df with models runs with no MPA implemented 
+#' @param scenarios df with model runs for all scenatios (implementation year, reserve size etc)
+#' @param type All for aggregate analysis, or fishery for fishery level analysis
+#' @return A dataframe with each scnario and respective transition, payoff periods and Biomass in year 50 in the reserve patches.
+
+payoff <- function(BAU, scenarios, type){
+  require(dplyr)
+  All <- inner_join(scenarios, BAU, by=c("Name", "Adjusted", "Year"))%>%data.frame()
+
+  if (type=="All"){
+   scen <- All %>% group_by(Adjusted, Implementation_year, Reserve_size, Year)%>%
+     summarise_at(.vars= vars(Catch, Biomass, PV, BAU_C, BAU_B, BAU_PV), .funs =  sum)%>%
+     arrange(Year, .by_group = TRUE)%>%
+     mutate (C_cum = cumsum(Catch), BAU_C_cum = cumsum(BAU_C))
+   
+   ?subset
+   
+   cross_point <- scen %>%  group_by(Adjusted, Implementation_year, Reserve_size)%>% 
+     subset(ifelse(Adjusted=="IUU_20_legal"|Adjusted=="IUU_40_legal"|Adjusted=="IUU_60_legal",     
+                   Catch >= BAU_C, Year >= Implementation_year & Catch >= BAU_C))%>% 
+              filter(Year==min(Year))%>%
+     mutate(Transition_period= Year, Loss = BAU_C_cum - C_cum)%>%
+     select(Adjusted, Implementation_year, Reserve_size, Transition_period, Loss)
+   
+   payoff_point <- inner_join(scen, cross_point, by=c("Adjusted", "Implementation_year", "Reserve_size")) %>%
+     mutate(Payoff = BAU_C_cum + Loss)%>%
+     group_by(Adjusted, Implementation_year, Reserve_size)%>%
+     subset(C_cum >= Payoff)%>%
+     filter(Year==min(Year))%>%
+     mutate(Payoff_period= Year)%>%
+     select(Adjusted, Implementation_year, Reserve_size, Payoff_period)
+   
+  t1 <- merge(cross_point, payoff_point, by=c("Adjusted", "Implementation_year", "Reserve_size"), all.x=TRUE)
+   
+  B_50 <- scen %>% group_by(Adjusted, Implementation_year, Reserve_size) %>%
+      filter(Year==max(Year))%>% mutate(B_per_50 = ((Biomass-BAU_B)/BAU_B)*100)
+    
+    payoff_analysis <- B_50 %>%
+      full_join(t1, by=c("Adjusted", "Implementation_year", "Reserve_size"))%>%
+      select(Adjusted, Implementation_year, Reserve_size, Transition_period, Payoff_period, B_per_50, Loss)
+  
+    output <- All %>%
+      group_by(Adjusted, Implementation_year, Reserve_size)%>%
+      summarise_at(.vars= vars(Catch, Biomass, PV, BAU_C, BAU_B, BAU_PV), .funs =  sum)%>%
+      mutate(C_change = Catch - BAU_C,
+                             B_change = Biomass - BAU_B, 
+                             PV_change = PV - BAU_PV, 
+                             C_per = (C_change/BAU_C)*100, 
+                             B_per = (B_change/BAU_B)*100,
+                             PV_per = (PV_change/BAU_PV)*100)%>%
+      select(Adjusted, Implementation_year, Reserve_size, C_change, B_change, PV_change, C_per, B_per, PV_per)%>%
+      merge(payoff_analysis, by= c("Adjusted", "Implementation_year", "Reserve_size"))
+    
+    } else {
+      scen <- All %>% group_by(Name, Adjusted, Implementation_year, Reserve_size)%>%
+        arrange(Year, .by_group = TRUE)%>%
+        mutate (C_cum = cumsum(Catch), BAU_C_cum = cumsum(BAU_C))
+      
+      cross_point <- scen %>%  group_by(Name, Adjusted, Implementation_year, Reserve_size)%>% 
+        subset(ifelse(Adjusted=="IUU_20_legal"|Adjusted=="IUU_40_legal"|Adjusted=="IUU_60_legal",     
+                      Catch >= BAU_C, Year >= Implementation_year & Catch >= BAU_C))%>% 
+        filter(Year==min(Year))%>%
+        mutate(Transition_period= Year, Loss = BAU_C_cum - C_cum)%>%
+        select(Name, Adjusted, Implementation_year, Reserve_size, Transition_period, Loss)
+      
+      payoff_point <- inner_join(scen, cross_point, by=c("Name","Adjusted", "Implementation_year", "Reserve_size")) %>%
+        mutate(Payoff = BAU_C_cum + Loss)%>%
+        group_by(Name, Adjusted, Implementation_year, Reserve_size)%>%
+        subset(C_cum >= Payoff)%>%
+        filter(Year==min(Year))%>%
+        mutate(Payoff_period= Year)%>%
+        select(Name, Adjusted, Implementation_year, Reserve_size, Payoff_period)
+      
+      t1 <- merge(cross_point, payoff_point, by=c("Name","Adjusted", "Implementation_year", "Reserve_size"), all.x=TRUE)
+      
+      B_50 <- scen %>% group_by(Name, Adjusted, Implementation_year, Reserve_size) %>%
+        filter(Year==max(Year))%>% mutate(B_per_50 = ((Biomass-BAU_B)/BAU_B)*100)
+      
+      payoff_analysis <- B_50 %>%
+        full_join(t1, by=c("Name","Adjusted", "Implementation_year", "Reserve_size"))%>%
+        select(Name ,Adjusted, Implementation_year, Reserve_size, Transition_period, Payoff_period, B_per_50, Loss)
+      
+      output <- All %>%
+        group_by(Name, Adjusted, Implementation_year, Reserve_size)%>%
+        summarise_at(.vars= vars(Catch, Biomass, PV, BAU_C, BAU_B, BAU_PV), .funs =  sum)%>%
+        mutate(C_change = Catch - BAU_C,
+               B_change = Biomass - BAU_B, 
+               PV_change = PV - BAU_PV, 
+               C_per = (C_change/BAU_C)*100, 
+               B_per = (B_change/BAU_B)*100,
+               PV_per = (PV_change/BAU_PV)*100)%>%
+        select(Name, Adjusted, Implementation_year, Reserve_size, C_change, B_change, PV_change, C_per, B_per, PV_per)%>%
+        merge(payoff_analysis, by= c("Name","Adjusted", "Implementation_year", "Reserve_size"))
+    }
+  
+  return(output)  
+}
